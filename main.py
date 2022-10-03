032cd854607390927797bb89db43a2b872c77510
IceMerman <jsjuanes51@gmail.com>
2022-10-02 19:11:18 -0500

    Iteration on templates

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from flask import (
 )
 
 app = Flask(__name__, template_folder='./templates')
+todo_list = [f'todo {i:>02}' for i in range(10)]
+todo_dict = {i: f'todo {i:>02}' for i in range(10)}
 
 @app.route('/')
 def index():
@@ -21,7 +23,12 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
     user_ip = escape(user_ip)
-    return render_template('hello.html', user_ip=user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos': todo_list,
+        'todo_dict': todo_dict,
+    }
+    return render_template('hello.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
